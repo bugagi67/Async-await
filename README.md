20b1317ebeee3c5b857b87d96be0ce0f954b5e00
Dmitriy Greshnov <dmitrygreshnov2@gmail.com>
2024-05-28 23:44:19 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Promises, async/await, timers & event loop
+
+# Async/await
+
+[![Build status](https://ci.appveyor.com/api/projects/status/tx0mik06f3hwfsa5?svg=true)](https://ci.appveyor.com/project/bugagi67/async-await)
---
+++ src/js/__test__/app.test.js
@@ -0,0 +1,17 @@
+import GameSavingLoader from '../GameSavingLoader';
+
+test('Testing the load() method', async () => {
+  const expected = {
+    id: 9,
+    created: 1546300800,
+    userInfo: {
+      id: 1,
+      name: 'Hitman',
+      level: 10,
+      points: 2000,
+    },
+  };
+
+  const result = await GameSavingLoader.load();
+  expect(result).toEqual(expected);
+});
